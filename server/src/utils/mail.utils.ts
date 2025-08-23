af45f2c94ee777d3310a985a5233b45c3e5e512d
ZombieGenZ <zombiegenzzz@gmail.com>
2025-08-23 13:26:48 +0700

    Thêm module yêu cầu báo giá & thêm module gửi mail

---
+++ server/src/utils/mail.utils.ts
@@ -0,0 +1,58 @@
+import nodemailer from 'nodemailer'
+import { OAuth2Client } from 'google-auth-library'
+import * as SMTPTransport from 'nodemailer/lib/smtp-transport'
+
+const GOOGLE_MAILER_CLIENT_ID = process.env.GOOGLE_MAILER_CLIENT_ID as string
+const GOOGLE_MAILER_CLIENT_SECRET = process.env.GOOGLE_MAILER_CLIENT_SECRET as string
+const GOOGLE_MAILER_REFRESH_TOKEN = process.env.GOOGLE_MAILER_REFRESH_TOKEN as string
+const GOOGLE_MAILER_EMAIL_PRIMARY_ADDRESS = process.env.GOOGLE_MAILER_EMAIL_PRIMARY_ADDRESS as string
+const GOOGLE_MAILER_EMAIL_SEND_ADDRESS = process.env.GOOGLE_MAILER_EMAIL_SEND_ADDRESS as string
+
+const MailClient = new OAuth2Client(GOOGLE_MAILER_CLIENT_ID, GOOGLE_MAILER_CLIENT_SECRET)
+
+MailClient.setCredentials({
+  refresh_token: GOOGLE_MAILER_REFRESH_TOKEN
+})
+
+export const sendMail = async (
+  to: string,
+  subject: string,
+  html: string,
+  fromEmail: string = GOOGLE_MAILER_EMAIL_SEND_ADDRESS
+) => {
+  try {
+    const access_token_object = await MailClient.getAccessToken()
+    const access_token = access_token_object?.token as string
+
+    const transportOptions: SMTPTransport.Options = {
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: GOOGLE_MAILER_EMAIL_PRIMARY_ADDRESS,
+        clientId: GOOGLE_MAILER_CLIENT_ID,
+        clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
+        refreshToken: GOOGLE_MAILER_REFRESH_TOKEN,
+        accessToken: access_token
+      }
+    }
+
+    const transport = nodemailer.createTransport(transportOptions)
+
+    const mailOptions: nodemailer.SendMailOptions = {
+      from: fromEmail,
+      to,
+      subject,
+      html,
+      attachments: []
+    }
+
+    await transport.sendMail(mailOptions)
+
+    return true
+  } catch (err) {
+    console.error('Error sending email:', err)
+    return false
+  }
+}
